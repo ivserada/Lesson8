d4dd51d7aaa3bb3b50bece40c4844909e41f8db1
ivserada <ivserada@gmail.com>
2026-09-17 21:18:50 +0300

    feat: tasks resolution

--- task3.ts
+++ task3.ts
@@ -1,5 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Напишите функцию, которая проверяет не содержит ли слово повторяющихся букв
 
 const str1 = "Android";
 const str2 = "Iphone";
+
+function hasUniqueLetters(word: string): boolean {
+  const letters = word.toLowerCase().split("");
+  return new Set(letters).size !== letters.length;
+}
+
+console.log(hasUniqueLetters(str1));
+console.log(hasUniqueLetters(str2));
